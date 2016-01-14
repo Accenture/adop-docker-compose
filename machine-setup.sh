7c1cadb565798cc8319ddc71a1c5c14bea9d0a67
Dan Tarleton <daniel.s.tarleton@accenture.com>
2016-01-14 10:27:48 +0000

    Fixed some issues in machine-setup that caused errors in the setup. Also added LOGSTASH_HOST.

--- machine-setup.sh
+++ machine-setup.sh
@@ -21,14 +21,15 @@ docker-machine create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY 
 
 eval "$(/usr/local/bin/docker-machine env $MACHINE_NAME)"
 export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
+export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
 docker-compose -f docker-compose-no-init.yml up -d
 
 sleep 10
 
-docker-machine ssh $MACHINE_NAME "mkdir -p /home/ubuntu/volumes; sudo mkdir -p $DIR/volumes"
-docker-machine scp -r /root/docker-compose/volumes/nginx-configuration-vol/ $MACHINE_NAME:/home/ubuntu/volumes/
-docker-machine scp -r /root/docker-compose/volumes/nginx-releasenote-vol/ $MACHINE_NAME:/home/ubuntu/volumes/
-docker-machine ssh $MACHINE_NAME "sudo su -c 'mv -f /home/ubuntu/volumes/nginx-configuration-vol $DIR/volumes; mv -f /home/ubuntu/volumes/nginx-releasenote-vol $DIR/volumes/; cd $DIR/volumes; chown -R 1000:1000 gerrit-site-vol jenkins-home-vol git-repos-vol'"
+docker-machine ssh $MACHINE_NAME "rm -rf /home/ubuntu/volumes/; mkdir -p /home/ubuntu/volumes; sudo rm -rf $DIR/volumes/nginx-configuration-vol/ $DIR/volumes/nginx-releasenote-vol/; sudo mkdir -p $DIR/volumes"
+docker-machine scp -r $DIR/volumes/nginx-configuration-vol/ $MACHINE_NAME:/home/ubuntu/volumes/
+docker-machine scp -r $DIR/volumes/nginx-releasenote-vol/ $MACHINE_NAME:/home/ubuntu/volumes/
+docker-machine ssh $MACHINE_NAME "sudo su -c 'mv -f /home/ubuntu/volumes/nginx-configuration-vol $DIR/volumes/; mv -f /home/ubuntu/volumes/nginx-releasenote-vol $DIR/volumes/; cd $DIR/volumes; chown -R 1000:1000 gerrit-site-vol jenkins-home-vol git-repos-vol'"
 
 docker exec dockercompose_ldap_1 bash -c '/usr/local/bin/load_ldif.sh -h $(hostname) -u cn=admin,dc=adop,dc=accenture,dc=com -p Sw4syJSWQRx2AK6KE3vbhpmL -b dc=adop,dc=accenture,dc=com -f /tmp/structure.ldif'
 
