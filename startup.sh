a0253a168419d268553ca79edc170917433510cd
Dan Tarleton <daniel.s.tarleton@accenture.com>
2016-01-22 16:01:06 +0000

    Updated startup.sh to take arguments including overrides as arguments

--- startup.sh
+++ startup.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-export MACHINE_NAME=$1
-export CUSTOM_NETWORK_NAME=$2
-export VOLUME_DRIVER=$3
-export AWS_ACCESS_KEY=$4
-export AWS_SECRET_ACCESS_KEY=$5
-export VPC_ID=$6
-export REGION=$7
+OVERRIDES=
 
 usage(){
-	echo "Usage: source startup.sh <MACHINE_NAME> <CUSTOM_NETWORK_NAME> <VOLUME_DRIVER> <AWS_ACCESS_KEY> <AWS_SECRET_ACCESS_KEY> <VPC_ID> <REGION>"
+	echo "Usage: 
+		./startup.sh \
+		  -m <MACHINE_NAME> \
+		  -n <CUSTOM_NETWORK_NAME> \
+		  -a <AWS_ACCESS_KEY> \
+		  -s <AWS_SECRET_ACCESS_KEY> \
+		  -c <VPC_ID> \
+		  -r <REGION> \
+		  -f path/to/override1.yml \
+		  -f path/to/override2.yml \
+		  ..."
 }
 
-if [ -z $MACHINE_NAME ] | \
-	[ -z $CUSTOM_NETWORK_NAME ] | \
-	[ -z $AWS_ACCESS_KEY ] | \
-	[ -z $AWS_SECRET_ACCESS_KEY ] | \
-	[ -z $VPC_ID ] | \
-	[ -z $REGION ]; then
-  usage
-  exit 1
-fi
+while getopts "m:n:a:s:c:r:f:" opt; do
+  case $opt in
+    m)
+      export MACHINE_NAME=${OPTARG}
+      ;;
+    n)
+      export CUSTOM_NETWORK_NAME=${OPTARG}
+      ;;
+    a)
+      export AWS_ACCESS_KEY=${OPTARG}
+      ;;
+    s)
+      export AWS_SECRET_ACCESS_KEY=${OPTARG}
+      ;;
+    c)
+      export VPC_ID=${OPTARG}
+      ;;
+    r)
+      export REGION=${OPTARG}
+      ;;
+    f)
+      export OVERRIDES="${OVERRIDES} -f ${OPTARG}"
+      ;;
+    *)
+      echo "Invalid parameter(s) or option(s)."
+      usage
+      ;;
+  esac
+done
 
 source env.config.sh
 
@@ -30,5 +54,8 @@ eval "$(/usr/local/bin/docker-machine env $MACHINE_NAME)"
 export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
 export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
 docker network create $CUSTOM_NETWORK_NAME
+docker-compose -f compose/elk.yml up -d
 docker-compose pull
-docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml up -d
+docker-compose -f docker-compose.yml ${OVERRIDES} up -d
+
+echo "Run this command in your shell: eval \"$(/usr/local/bin/docker-machine env $MACHINE_NAME)\""
