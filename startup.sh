1fcae460ee55ddbc5287c150adc2341c4e3e4ebc
Dan Tarleton <daniel.s.tarleton@accenture.com>
2016-01-22 14:30:55 +0000

    startup.sh accepted to take VPC and Region as variables

--- startup.sh
+++ startup.sh
@@ -5,41 +5,28 @@ export CUSTOM_NETWORK_NAME=$2
 export VOLUME_DRIVER=$3
 export AWS_ACCESS_KEY=$4
 export AWS_SECRET_ACCESS_KEY=$5
-
-DIR=$(pwd)/docker-compose
+export VPC_ID=$6
+export REGION=$7
 
 usage(){
-	echo "Usage: source startup.sh <MACHINE_NAME> <CUSTOM_NETWORK_NAME> <VOLUME_DRIVER> <AWS_ACCESS_KEY> <AWS_SECRET_ACCESS_KEY>"
+	echo "Usage: source startup.sh <MACHINE_NAME> <CUSTOM_NETWORK_NAME> <VOLUME_DRIVER> <AWS_ACCESS_KEY> <AWS_SECRET_ACCESS_KEY> <VPC_ID> <REGION>"
 }
 
-if [ -z $AWS_ACCESS_KEY ]; then
-  echo "AWS_ACCESS_KEY not set"
+if [ -z $MACHINE_NAME ] | \
+	[ -z $CUSTOM_NETWORK_NAME ] | \
+	[ -z $AWS_ACCESS_KEY ] | \
+	[ -z $AWS_SECRET_ACCESS_KEY ] | \
+	[ -z $VPC_ID ] | \
+	[ -z $REGION ]; then
   usage
   exit 1
 fi
 
-if [ -z $AWS_SECRET_ACCESS_KEY ]; then
-  echo "AWS_SECRET_ACCESS_KEY not set"
-  usage
-  exit 1
-fi
-
-if [ -z $MACHINE_NAME ]; then
-  echo "MACHINE_NAME not set"
-  usage
-  exit 1
-fi
-
-if [ -z $CUSTOM_NETWORK_NAME ]; then
-  echo "CUSTOM_NETWORK_NAME not set"
-  usage
-  exit 1
-fi
-
-docker-machine create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY --amazonec2-vpc-id vpc-27d43742 --amazonec2-instance-type t2.large --amazonec2-region eu-west-1 $MACHINE_NAME
+docker-machine create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY --amazonec2-vpc-id $VPC_ID --amazonec2-instance-type t2.large --amazonec2-region $REGION $MACHINE_NAME
 
 eval "$(/usr/local/bin/docker-machine env $MACHINE_NAME)"
 export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
 export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
 docker network create $CUSTOM_NETWORK_NAME
-docker-compose -f docker-compose/docker-compose.yml -f docker-compose/etc/volumes/${VOLUME_DRIVER}/default.yml up -d
+docker-compose pull
+docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml up -d
