e9f4c47c25c22433e534a414b82772b7542ffbde
Markos Rendell <markosrendell@gmail.com>
2016-01-25 22:04:41 +0000

    Marking startup.sh create our standard stack with the minimal required inputs.

--- startup.sh
+++ startup.sh
@@ -1,12 +1,18 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 OVERRIDES=
 
 usage(){
-	echo "Usage: ./startup.sh -m <MACHINE_NAME> -n <CUSTOM_NETWORK_NAME> -a <AWS_ACCESS_KEY> -s <AWS_SECRET_ACCESS_KEY> -c <VPC_ID> -r <REGION> -f path/to/override1.yml -f path/to/override2.yml ..."
+	echo "Usage: ./startup.sh -m <MACHINE_NAME> -a <AWS_ACCESS_KEY> -s <AWS_SECRET_ACCESS_KEY> -c <VPC_ID> -r <REGION> -v <VOLUME_DRIVER> -n <CUSTOM_NETWORK_NAME>(optional) -l LOGGING_DRIVER(optional) -f path/to/additional_override1.yml(optional) -f path/to/additional_override2.yml(optional) ..."
 }
 
-while getopts "m:n:a:s:c:r:f:" opt; do
+#Defaults
+VOLUME_DRIVER=local
+LOGGING_OVERRIDE=' -f etc/logging/syslog/default.yml'
+CUSTOM_NETWORK_NAME=adopnetwork
+
+
+while getopts "m:n:a:s:c:r:f:v:l:" opt; do
   case $opt in
     m)
       export MACHINE_NAME=${OPTARG}
@@ -26,8 +32,14 @@ while getopts "m:n:a:s:c:r:f:" opt; do
     r)
       export REGION=${OPTARG}
       ;;
+    v)
+      export VOLUME_DRIVER=${OPTARG}
+      ;;
     f)
       export OVERRIDES="${OVERRIDES} -f ${OPTARG}"
+      ;;
+    l)
+      export LOGGING_OVERRIDE=" -f ${OPTARG}"
       ;;
     *)
       echo "Invalid parameter(s) or option(s)."
@@ -49,12 +61,15 @@ fi
 source env.config.sh
 
 docker-machine create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY --amazonec2-vpc-id $VPC_ID --amazonec2-instance-type t2.large --amazonec2-region $REGION $MACHINE_NAME
-eval "$(/usr/local/bin/docker-machine env $MACHINE_NAME)"
+
+eval "$(docker-machine env $MACHINE_NAME)"
 export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
 export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
 docker network create $CUSTOM_NETWORK_NAME
+docker-compose -f compose/elk.yml pull
+docker-compose -f docker-compose.yml ${OVERRIDES} -f etc/volumes/${VOLUME_DRIVER}/default.yml $LOGGING_OVERRIDE pull
+set -x
 docker-compose -f compose/elk.yml up -d
-docker-compose pull
-docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml -f etc/logging/syslog/default.yml up -d
-echo Your ADOP is ready.  
-echo You will need to update the docker-machine security group to permit inbound http traffic on port 80
+set +x
+
+echo "Run this command in your shell: eval \"$(docker-machine env $MACHINE_NAME)\""
