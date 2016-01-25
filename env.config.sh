16735292739147f41180d41e0ab8402dbbcf1534
Quirino Brizi <quirino.brizi@accenture.com>
2016-01-25 14:57:33 +0000

    Update as per comments

--- env.config.sh
+++ env.config.sh
@@ -1,63 +1,36 @@
 #!/bin/bash
 
-# Proxy
+# Globals
 
-export LDAP_SERVER="ldap:389"
-export LDAP_PASSWORD="***REMOVED***"
+export LDAP_PWD="***REMOVED***"
 
 # LDAP
 
-export LDAP_ADMIN_PASSWORD="***REMOVED***"
 export LDAP_LOG_LEVEL=0
 
 # Gerrit MySQL
 
-export MYSQL_ROOT_PASSWORD="***REMOVED***"
-export MYSQL_USER="gerrit"
-export MYSQL_PASSWORD="***REMOVED***"
-export MYSQL_DATABASE="gerrit"
+export GERRIT_MYSQL_USER="gerrit"
+export GERRIT_MYSQL_PASSWORD="***REMOVED***"
+export GERRIT_MYSQL_DATABASE="gerrit"
 
 # Gerrit
 
-export DB_PORT="3306"
-export DB_NAME="gerrit"
-export DB_USER="gerrit"
-export DB_PASSWORD="***REMOVED***"
-export LDAP_PASSWORD="***REMOVED***"
+export DB_NAME=${GERRIT_MYSQL_DATABASE}
+export DB_USER=${GERRIT_MYSQL_USER}
+export DB_PASSWORD=${GERRIT_MYSQL_PASSWORD}
 export USER_NAME="Gerrit Code Review" 
 export USER_EMAIL="gerrit@adop"
 
 # Sonar MySQL
 
-export MYSQL_ROOT_PASSWORD="***REMOVED***"
-export MYSQL_USER="sonar"
-export MYSQL_PASSWORD="***REMOVED***"
-export MYSQL_DATABASE="sonar"
-
-# Sonar
-
-export LDAP_BIND_PASSWORD="***REMOVED***"
+export SONAR_MYSQL_USER="sonar"
+export SONAR_MYSQL_PASSWORD="***REMOVED***"
+export SONAR_MYSQL_DATABASE="sonar"
 
 # Jenkins
 
-export LDAP_MANAGER_PASSWORD="***REMOVED***"
 export SONAR_ACCOUNT_LOGIN="jenkins"
 export SONAR_ACCOUNT_PASSWORD="***REMOVED***"
-export SONAR_DB_LOGIN="sonar"
-export SONAR_DB_PASSWORD="***REMOVED***"
-export SONAR_PLUGIN_VERSION=""
-export SONAR_ADDITIONAL_PROPS=""
-export SONAR_RUNNER_VERSION="2.4"
-export ANT_VERSION="1.9.4"
-export MAVEN_VERSION="3.0.5"
-export NODEJS_VERSION="0.12.2"
-export NODEJS_GLOBAL_PACKAGES="grunt-cli@~0.1.13 bower@~1.3.12 plato@~1.2.1"
-export NODEJS_PACKAGES_REFRESH_HOURS="72"
-
-# Logstash
-
-export LS_HEAP_SIZE=1024m
-
-# Nexus
-
-export LDAP_BIND_PASSWORD="***REMOVED***"
+export SONAR_DB_LOGIN=${SONAR_MYSQL_USER}
+export SONAR_DB_PASSWORD=${SONAR_MYSQL_PASSWORD}
