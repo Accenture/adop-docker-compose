5efc7f81192bc6caa3ee847953c4c0ed5dde19f3
Dimitrov, Georgi <georgi.dimitrov@accenture.com>
2016-04-10 23:07:31 +0100

    changes so registry can generate certs fine

--- startup.sh
+++ startup.sh
@@ -103,7 +103,7 @@ fi
 # Run the Docker compose commands
 export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
 export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
-export HOST_IP=$(docker-machine ip $MACHINE_NAME)
+export HOST_IP=$(docker-machine inspect --format='{{.Driver.PrivateIPAddress}}' $MACHINE_NAME)
 docker-compose -f compose/elk.yml pull
 docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml $LOGGING_OVERRIDE ${OVERRIDES} pull
 docker-compose -f compose/elk.yml up -d
