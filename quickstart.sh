bd82bb8f546178fe2e90a6d7789a7cc0657a7e39
Gary A. Stafford <garystafford@rochester.rr.com>
2017-07-01 22:36:52 -0400

    Adding engine-install-url to fix docker.service error

--- quickstart.sh
+++ quickstart.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -e
 
-echo ' 
-      ###    ########   #######  ########  
-     ## ##   ##     ## ##     ## ##     ## 
-    ##   ##  ##     ## ##     ## ##     ## 
-   ##     ## ##     ## ##     ## ########  
-   ######### ##     ## ##     ## ##        
-   ##     ## ##     ## ##     ## ##        
-   ##     ## ########   #######  ##        
+echo '
+      ###    ########   #######  ########
+     ## ##   ##     ## ##     ## ##     ##
+    ##   ##  ##     ## ##     ## ##     ##
+   ##     ## ##     ## ##     ## ########
+   ######### ##     ## ##     ## ##
+   ##     ## ##     ## ##     ## ##
+   ##     ## ########   #######  ##
 '
 
 usage(){
    cat <<END_USAGE
 
 Usage:
-        ./quickstart.sh 
-	    -t local 
-	    [-m <MACHINE_NAME>] 
-	    [-u <INITIAL_ADMIN_USER>] 
+        ./quickstart.sh
+	    -t local
+	    [-m <MACHINE_NAME>]
+	    [-u <INITIAL_ADMIN_USER>]
 	    [-p <INITIAL_ADMIN_PASSWORD>]
 
-        ./quickstart.sh 
-	    -t aws 
-	    -m <MACHINE_NAME> 
-	    -c <AWS_VPC_ID> 
-	    -r <AWS_DEFAULT_REGION> 
-	    [-z <AVAILABILITY_ZONE_LETTER>] 
-	    [-a <AWS_ACCESS_KEY>] 
-	    [-s <AWS_SECRET_ACCESS_KEY>] 
-	    [-u <INITIAL_ADMIN_USER>] 
+        ./quickstart.sh
+	    -t aws
+	    -m <MACHINE_NAME>
+	    -c <AWS_VPC_ID>
+	    -r <AWS_DEFAULT_REGION>
+	    [-z <AVAILABILITY_ZONE_LETTER>]
+	    [-a <AWS_ACCESS_KEY>]
+	    [-s <AWS_SECRET_ACCESS_KEY>]
+	    [-u <INITIAL_ADMIN_USER>]
 	    [-p <INITIAL_ADMIN_PASSWORD>]
 
 END_USAGE
@@ -55,16 +55,16 @@ provision_local() {
     set -e
 
 }
-    
+
 # Function to create AWS-specific environment variables file
-source_aws() {  
+source_aws() {
   AWS_FILE='./conf/provider/env.provider.aws.sh'
 
-  if [ -f ${AWS_FILE} ]; then  
+  if [ -f ${AWS_FILE} ]; then
     echo "Your AWS parameters file already exists, deleting it..."
     rm -f ${AWS_FILE}
   fi
-    
+
   echo "Creating a new AWS variables file..."
   cp ./conf/provider/examples/env.provider.aws.sh.example ${AWS_FILE}
 
@@ -72,7 +72,7 @@ source_aws() {
   if [ -z ${AWS_VPC_ID} ]; then
     usage
     exit 1
-  else    
+  else
     sed -i'' -e "s/###AWS_VPC_ID###/$AWS_VPC_ID/g" ${AWS_FILE}
   fi
 
@@ -93,7 +93,7 @@ source_aws() {
   else
     sed -i'' -e "s/###AWS_SUBNET_ID###/$AWS_SUBNET_ID/g" ${AWS_FILE}
   fi
-  
+
   sed -i'' -e "s/###AWS_DEFAULT_REGION###/$AWS_DEFAULT_REGION/g" ${AWS_FILE}
 
 }
@@ -124,7 +124,7 @@ provision_aws() {
     then
       echo "WARN: AWS_SECRET_ACCESS_KEY not set (externally or with -s), delegating to Docker Machine"
     fi
-    
+
     if [ -z ${AWS_DOCKER_MACHINE_SIZE} ]; then
     	export AWS_DOCKER_MACHINE_SIZE="m4.xlarge"
     fi
@@ -138,10 +138,10 @@ provision_aws() {
     # Create Docker machine if one doesn't already exist with the same name
     docker-machine ip ${MACHINE_NAME} > /dev/null 2>&1
     rc=$?
-    
+
     # Reenable errexit
     set -e
-    
+
     if [ ${rc} -eq 0 ]; then
         echo "Docker machine '$MACHINE_NAME' already exists"
     else
@@ -150,7 +150,8 @@ provision_aws() {
                     --amazonec2-vpc-id ${AWS_VPC_ID} \
                     --amazonec2-zone $VPC_AVAIL_ZONE \
                     --amazonec2-instance-type ${AWS_DOCKER_MACHINE_SIZE} \
-                    --amazonec2-root-size ${AWS_ROOT_SIZE:-32}"
+                    --amazonec2-root-size ${AWS_ROOT_SIZE:-32} \
+                    --engine-install-url=https://web.archive.org/web/20170623081500/https://get.docker.com"
 
         if [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_DEFAULT_REGION}" ]; then
             MACHINE_CREATE_CMD="${MACHINE_CREATE_CMD} \
@@ -192,7 +193,7 @@ while getopts "t:m:a:s:c:z:r:u:p:" opt; do
       ;;
     p)
       export INITIAL_ADMIN_PASSWORD_PLAIN=${OPTARG}
-      ;;      
+      ;;
     *)
       echo "Invalid parameter(s) or option(s)."
       usage
